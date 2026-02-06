67407c4830b8041ffffcca5523a4e37b7b3515d1
nerdynoah <wwwgreatdog@gmail.com>
2026-02-06 16:07:38 -0500

    Added ardiono file

---
+++ RemoteMafia.ino
@@ -0,0 +1,26 @@
+#include <Keyboard.h>
+#include <KeyboardLayout.h>
+#include <Keyboard_da_DK.h>
+#include <Keyboard_de_DE.h>
+#include <Keyboard_es_ES.h>
+#include <Keyboard_fr_FR.h>
+#include <Keyboard_hu_HU.h>
+#include <Keyboard_it_IT.h>
+#include <Keyboard_pt_PT.h>
+#include <Keyboard_sv_SE.h>
+
+#include <EEPROM.h>
+
+#include <LiquidCrystal.h>
+
+int players = 6;
+
+void setup() {
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  
+
+}
